7dac51930b3b1c19c81fc6c5ae1018964d6935da
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-11-27 15:40:00 +0530

    Angular Notes

--- Session-5/angularapp/src/app/app.component.spec.ts
+++ Session-5/angularapp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myapp'`, () => {
+  it(`should have as title 'angularapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myapp');
+    expect(app.title).toEqual('angularapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('myapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angularapp app is running!');
   });
 });
