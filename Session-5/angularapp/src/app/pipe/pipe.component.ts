27030a76b431c065c5a083731bb1df78f35cfb1b
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2023-11-28 15:26:31 +0530

    Angular Routing,pipes

--- Session-5/angularapp/src/app/pipe/pipe.component.ts
+++ Session-5/angularapp/src/app/pipe/pipe.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class PipeComponent {
 
+
+  name:string="My Name is Nikunj, i am a JAVA FSD Trainer";
+  pi:number=3.1415927;
+  a=0.12345;
+  b=9876.5432;
+
+  today=new Date();
+
+  object={name:"Nikunj",email:"Nikunj@gmail.com",address:"India"};
+
+
 }
